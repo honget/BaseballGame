42248f0b139839342e255e74052d0fdef278294f
Hong <bilre84@naver.com>
2019-05-19 11:04:21 +0900

    주석 추가

--- app/src/main/java/com/example/baseballgame/MainActivity.java
+++ app/src/main/java/com/example/baseballgame/MainActivity.java
@@ -36,10 +36,13 @@ public class MainActivity extends BaseActivity {
         act.inputBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //유저가 입력 값 등록
                 chatList.add(new Chat(true, act.userInputEdt.getText().toString()));
+
                 mChatAdapter.notifyDataSetChanged();
                 act.messageListView.smoothScrollToPosition(chatList.size()-1);
+
+                //정답 확인 로직 호출
                 checkStrikeAndBalls();
             }
         });
@@ -47,9 +50,12 @@ public class MainActivity extends BaseActivity {
 
     void checkStrikeAndBalls() {
 
+        //유저가 입력값 배열
         int[] userInputArray = new int[3];
+        // 유저 입력 값
         int number = Integer.parseInt(act.userInputEdt.getText().toString());
 
+        // 유저 입력값 배열에 담는작업
         userInputArray[0] = number / 100;
         userInputArray[1] = number / 10 % 10;
         userInputArray[2] = number % 10;
@@ -57,10 +63,12 @@ public class MainActivity extends BaseActivity {
         int strikeCount = 0;
         int ballCount = 0;
 
+        // 정답 확인 로직
         for (int i=0 ; i < 3 ; i++) {
             for (int j=0 ; j < 3 ; j++) {
-
+                //숫자 존재 확인
                 if (userInputArray[i] == computerExamArray[j]) {
+                    //숫자 존재 & 위치 확인
                     if (i == j) {
                         strikeCount++;
                     }
@@ -71,30 +79,38 @@ public class MainActivity extends BaseActivity {
             }
         }
 
+        //run 에 들어갈경우 final 처리 필요
         final int strikeFinalCount = strikeCount;
         final int ballFinalCount = ballCount;
 
         if (strikeCount == 3) {
+            //모두 마출 경우 축하 메세지 등록
 //            Toast.makeText(mContext, "정답입니다! 축하합니다!", Toast.LENGTH_SHORT).show();
             chatList.add(new Chat(false,  String.format("정답입니다! 축하합니다! %s 번에 마추었습니다.", (int)(chatList.size()/2  + 1) ) ) );
 
         }
         else {
+            //몇개 마추었는지 메세지 등록
 //            Toast.makeText(mContext, String.format("%dS, %dB 입니다.", strikeCount, ballCount), Toast.LENGTH_SHORT).show();
             chatList.add(new Chat(false, String.format("%dS, %dB 입니다.", strikeFinalCount, ballFinalCount)));
         }
 
+        // 사용자 입력 창 초기화
         act.userInputEdt.setText("");
 
+        // 일부로 딜레이 주어 컴퓨터가 계산하고 있느 척 하기
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
+                //리스트 뷰 채인치 처리
                 mChatAdapter.notifyDataSetChanged();
+
+                //리스트 뷰 맨 아래로 위치 변경
                 act.messageListView.smoothScrollToPosition(chatList.size()-1);
 
             }
-        }, 1000);
+        }, 800);
     }
 
     @Override
@@ -106,27 +122,37 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    // 램덤 숫자 생성
     void makeExam() {
 
+        // 중복 0 없는 경우에 탈출 예정
         while (true) {
+
+            // 랜덤 숫자 생성
             int randomNumber = (int) (Math.random() * 899 + 100); // Ex. 747
 
+            // 랜덤 숫자 담을 배열
             int[] tempNumber = new int[3];
 
             tempNumber[0] = randomNumber / 100;
             tempNumber[1] = randomNumber / 10 % 10;
             tempNumber[2] = randomNumber % 10;
 
+            // 중복 값 존재 여부 확인
             boolean isDuplOk = true;
+
             if (tempNumber[0] == tempNumber[1] || tempNumber[1] == tempNumber[2] || tempNumber[0] == tempNumber[2]) {
                 isDuplOk = false;
             }
 
+            // 0 있는지 확인
             boolean isZeroOk = true;
+
             if (tempNumber[0] == 0 || tempNumber[1] == 0 || tempNumber[2] == 0) {
                 isZeroOk = false;
             }
 
+            // 중복 0 없는 경우에 탈출!!
             if (isDuplOk && isZeroOk) {
                 computerExamArray[0] = tempNumber[0];
                 computerExamArray[1] = tempNumber[1];
